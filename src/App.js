e29982af990ed0d2a89241a6817c4a0f02570ddc
kiran <kiran.gopi@bourntec.com>
2022-12-20 16:02:45 +0530

    kiran

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+
+import { useState } from 'react';
 import Entries from './component/Entries';
+
 function App() {
+  const [components, setComponents] = useState(["Sample Component"]); 
+
+  function addComponent() {
+    setComponents([...components, "kishan"]) 
+    
+  } 
   return (
     <div className="App">
      <Entries/> 
+     <div> 
+
+    
+  </div> 
     </div>
   );
 }
