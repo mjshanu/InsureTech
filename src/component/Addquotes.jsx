43e6d39a861788f5e5d8ae17d61ab6829642ecc7
mjshanu <mjshanukk@gmail.com>
2022-12-20 16:11:26 +0530

    shanu

--- src/component/Addquotes.jsx
+++ src/component/Addquotes.jsx
@@ -23,7 +23,37 @@ export default function Addquotes() {
           <option value="">AM</option>
           <option value="">PM</option>
         </select>
-         
+        <br></br>
+        <br></br>
+       <h1>Requested Contract and Products</h1>  
+       <div >
+        <label>contract</label>
+        <br></br>
+       <select>
+          <option value="">select</option>
+          
+        </select>
+        </div>
+        <div>
+        <br></br>
+        <label>product</label>
+        <br></br>
+        <select>
+          <option value="">select</option>
+          
+        </select>
+        </div>
+        <div>
+        <br></br>
+        <label>Marketing Referal</label>
+        <br></br>
+        <select>
+          <option value="">None</option>
+          
+        </select>
+</div>
+
+
         
       </form>
 
